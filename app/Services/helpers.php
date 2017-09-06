872ad598b4650fe7d47d07c2790640519c2190e2
mohsenjalalian <jalalianmohsen@gmail.com>
2017-09-06 14:07:00 +0430

    helper

--- app/Services/helpers.php
+++ app/Services/helpers.php
@@ -10,7 +10,8 @@
  * @param $str
  * @return mixed
  */
-function translateDigits($str) {
+function translateDigits($str)
+{
     $newString = str_replace(
         ['0','1','2','3','4','5','6','7','8','9'],
         ['۰','۱','۲','۳','۴','۵','۶','۷','۸','۹'],
